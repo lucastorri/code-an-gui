3c9591f56fcb9fb9de0e2c934aa3b798583a2305
Lucas Torri <lucastorri@gmail.com>
2012-11-08 15:14:12 -0200

    adding workspaces that a project most touches

---
+++ public/visualizer/commitsPerWorkspacePerProject.js
@@ -0,0 +1,76 @@
+define(function() {
+    return function(element, project) {
+        project = project.toUpperCase();
+        an.data("co.torri.dod.analysis.CommitsPerWorkspacePerProjectAnalyzer", function(data, desc) {
+
+            var h3 = d3.select("#"+element.attr("id")).append("h3")
+              h3.text(desc);
+
+            data = data.filter(function(e) {return e.project == project; }).slice(0,8);
+
+            console.log(data);
+
+            var margin = {top: 10, right: 10, bottom: 30, left: 40},
+                width = element.width() - margin.left - margin.right,
+                height = element.height() - margin.top - margin.bottom;
+
+            var formatPercent = d3.format("");
+
+            var x = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+
+            var y = d3.scale.linear()
+                .range([height, 0]);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom");
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .tickFormat(formatPercent);
+
+            var svg = d3.select("#"+element.attr("id")).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+            data.forEach(function(d) {
+                d.checkins = +d.checkins;
+            });
+
+            x.domain(data.map(function(d) { return d.workspace; }));
+            y.domain([0, d3.max(data, function(d) { return d.checkins; })]);
+
+            svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+            svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Checkins");
+
+            svg.selectAll(".bar")
+              .data(data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("x", function(d) { return x(d.workspace); })
+              .attr("width", x.rangeBand())
+              .attr("y", function(d) { return y(d.checkins); })
+              .attr("height", function(d) { return height - y(d.checkins); });
+
+
+
+        });
+    };
+});
